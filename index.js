70611b55e64f0b9db1c717e61fc7e369ec1de5cb
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-06 22:45:44 +0100

    chore: Add project dependencies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const hamburger = document.querySelector(".menu-bars");
-const mobileMenu = document.querySelector(".mobile-nav")
-const closeButton = document.querySelector(".close")
+const hamburger = document.querySelector('.menu-bars');
+const mobileMenu = document.querySelector('.mobile-nav');
+const closeButton = document.querySelector('.close');
 
-hamburger.addEventListener("click", () => {
-  mobileMenu.classList.add("change")
-})
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.add('change');
+});
 
-closeButton.addEventListener("click", () => {
-  mobileMenu.classList.remove("closed")
-})
+closeButton.addEventListener('click', () => {
+  mobileMenu.classList.remove('closed');
+});
