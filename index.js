6f885e3f651ee0b9fa46b167fac63deae6432072
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-10-06 22:03:58 +0100

    feat: Make mobile menu nav responsive

---
+++ index.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector(".menu-bars");
+const mobileMenu = document.querySelector(".mobile-nav")
+const closeButton = document.querySelector(".close")
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.add("change")
+})
+
+closeButton.addEventListener("click", () => {
+  mobileMenu.classList.remove("closed")
+})
